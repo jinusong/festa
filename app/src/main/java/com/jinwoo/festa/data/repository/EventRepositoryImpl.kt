682b36839d63029a11a6a40af37c0389c7cba146
jinusong <sugwer7464@gmail.com>
2019-06-26 23:10:44 +0900

    [Update] event repository

--- app/src/main/java/com/jinwoo/festa/data/repository/EventRepositoryImpl.kt
+++ app/src/main/java/com/jinwoo/festa/data/repository/EventRepositoryImpl.kt
@@ -1,2 +1,13 @@
 package com.jinwoo.festa.data.repository
 
+import com.jinwoo.festa.data.datasource.EventDataSource
+import com.jinwoo.festa.data.mapper.EventDataMapper
+import com.jinwoo.festa.domain.entity.EventEntity
+import com.jinwoo.festa.domain.entity.EventRepository
+import io.reactivex.Flowable
+
+class EventRepositoryImpl(val datasource: EventDataSource, val eventDataMapper: EventDataMapper): EventRepository {
+    override fun getEventList(pageSize: Int): Flowable<ArrayList<EventEntity>>
+            = datasource.getRemoteEventList(pageSize).map {
+        ArrayList(it.rows.map { eventDataMapper.mapFrom(it) })}
+}
