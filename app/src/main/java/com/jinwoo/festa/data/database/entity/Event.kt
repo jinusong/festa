243058baae648b24b72f0ac5442c8c03faa9e2d4
jinusong <sugwer7464@gmail.com>
2019-07-01 17:24:07 +0900

    [Fix] Event Key
    Set the Event PrimaryKey

--- app/src/main/java/com/jinwoo/festa/data/database/entity/Event.kt
+++ app/src/main/java/com/jinwoo/festa/data/database/entity/Event.kt
@@ -2,13 +2,10 @@ package com.jinwoo.festa.data.database.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.jinwoo.festa.data.entity.HostData
-import com.jinwoo.festa.data.entity.LocationData
-import com.jinwoo.festa.data.entity.MetadataData
-import com.jinwoo.festa.data.entity.TicketData
 
 @Entity
 data class Event(
+    @PrimaryKey
     val eventId: Int,
     val eventName: String,
     var eventDate: String,
