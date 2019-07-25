46cc0a533bae393cc52657c7ca383e301d46b006
jinusong <sugwer7464@gmail.com>
2019-07-25 19:56:13 +0900

    [Update] Dagger Modules of Event
    Update the code of the Dagger Modules with additional dependencies.

--- app/src/main/java/com/jinwoo/festa/presentation/di/module/eventdetail/EventDetailModule.kt
+++ app/src/main/java/com/jinwoo/festa/presentation/di/module/eventdetail/EventDetailModule.kt
@@ -1,15 +1,128 @@
 package com.jinwoo.festa.presentation.di.module.eventdetail
 
+import android.content.Context
+import androidx.room.Room
+import com.jinwoo.festa.data.Api
+import com.jinwoo.festa.data.database.EventDatabase
+import com.jinwoo.festa.data.database.LikeDatabase
+import com.jinwoo.festa.data.database.TicketDatabase
+import com.jinwoo.festa.data.database.dao.EventDao
+import com.jinwoo.festa.data.database.dao.LikeDao
+import com.jinwoo.festa.data.database.dao.TicketDao
+import com.jinwoo.festa.data.datasource.*
+import com.jinwoo.festa.data.mapper.EventDataMapper
+import com.jinwoo.festa.data.mapper.LikeDataMapper
+import com.jinwoo.festa.data.mapper.TicketDataMapper
+import com.jinwoo.festa.data.repository.EventRepositoryImpl
+import com.jinwoo.festa.data.repository.LikeRepositoryImpl
+import com.jinwoo.festa.data.repository.TicketRepositoryImpl
+import com.jinwoo.festa.domain.entity.*
+import com.jinwoo.festa.domain.usecase.GetEventDetailUseCase
+import com.jinwoo.festa.domain.usecase.SaveLikeUseCase
 import com.jinwoo.festa.presentation.di.scope.ActivityScope
 import com.jinwoo.festa.presentation.eventdetail.EventDetailContract
 import com.jinwoo.festa.presentation.eventdetail.EventDetailPresenter
+import com.jinwoo.festa.presentation.mapper.EventModelMapper
 import dagger.Module
 import dagger.Provides
+import io.reactivex.disposables.CompositeDisposable
 
 @Module
 class EventDetailModule {
     @ActivityScope
     @Provides
-    fun providePresenter(): EventDetailContract.Presenter
-            = EventDetailPresenter()
+    fun provideEventDatabase(context: Context): EventDatabase
+            = Room.databaseBuilder(context, EventDatabase::class.java, "event.db").build()
+
+    @ActivityScope
+    @Provides
+    fun provideLikeDatabase(context: Context): LikeDatabase
+            = Room.databaseBuilder(context, LikeDatabase::class.java, "like.db").build()
+
+    @ActivityScope
+    @Provides
+    fun provideTicketDatabase(context: Context): TicketDatabase
+            = Room.databaseBuilder(context, TicketDatabase::class.java, "ticket.db").build()
+
+    @ActivityScope
+    @Provides
+    fun provideEventDao(database: EventDatabase): EventDao = database.eventDao
+
+    @ActivityScope
+    @Provides
+    fun provideLikeDao(database: LikeDatabase): LikeDao = database.likeDao
+
+    @ActivityScope
+    @Provides
+    fun provideTicketDao(database: TicketDatabase): TicketDao = database.ticketDao
+
+    @ActivityScope
+    @Provides
+    fun provideEventDataSource(api: Api, eventDao: EventDao): EventDataSource
+            = EventDataSourceImpl(api, eventDao)
+
+    @ActivityScope
+    @Provides
+    fun provideLikeDataSource(likeDao: LikeDao): LikeDataSource
+            = LikeDataSourceImpl(likeDao)
+
+    @ActivityScope
+    @Provides
+    fun provideTicketDataSource(ticketDao: TicketDao): TicketDataSource
+            = TicketDataSourceImpl(ticketDao)
+
+    @ActivityScope
+    @Provides
+    fun provideEventRepository(eventDataSource: EventDataSource, eventDataMapper: EventDataMapper): EventRepository
+            = EventRepositoryImpl(eventDataSource, eventDataMapper)
+
+    @ActivityScope
+    @Provides
+    fun provideLikeRepository(likeDataSource: LikeDataSource, likeDataMapper: LikeDataMapper): LikeRepository
+            = LikeRepositoryImpl(likeDataSource, likeDataMapper)
+
+    @ActivityScope
+    @Provides
+    fun provideTicketRepository(ticketDataSource: TicketDataSource, ticketDataMapper: TicketDataMapper): TicketRepository
+            = TicketRepositoryImpl(ticketDataSource, ticketDataMapper)
+
+    @ActivityScope
+    @Provides
+    fun provideEventService(eventRepository: EventRepository,
+                            likeRepository: LikeRepository, ticketRepository: TicketRepository): EventService
+            = EventServiceImpl(eventRepository, likeRepository, ticketRepository)
+
+    @ActivityScope
+    @Provides
+    fun provideGetEventDetailUseCase(eventService: EventService, composite: CompositeDisposable)
+            = GetEventDetailUseCase(eventService, composite)
+
+    @ActivityScope
+    @Provides
+    fun provideSaveLikeUseCase(eventService: EventService, composite: CompositeDisposable)
+            = SaveLikeUseCase(eventService, composite)
+
+    @ActivityScope
+    @Provides
+    fun provideEventDataMapper(): EventDataMapper = EventDataMapper()
+
+    @ActivityScope
+    @Provides
+    fun provideLikeDataMapper(): LikeDataMapper = LikeDataMapper()
+
+    @ActivityScope
+    @Provides
+    fun provideEventModelMapper(): EventModelMapper = EventModelMapper()
+
+    @ActivityScope
+    @Provides
+    fun provideTicketDataMapper() = TicketDataMapper()
+
+    @ActivityScope
+    @Provides
+    fun providePresenter(getEventListUseCase: GetEventDetailUseCase,
+                         saveLikeUseCase: SaveLikeUseCase,
+                         eventModelMapper: EventModelMapper
+    ): EventDetailContract.Presenter
+            = EventDetailPresenter(getEventListUseCase, saveLikeUseCase, eventModelMapper)
 }
--- app/src/main/java/com/jinwoo/festa/presentation/di/module/main/MainModule.kt
+++ app/src/main/java/com/jinwoo/festa/presentation/di/module/main/MainModule.kt
@@ -3,13 +3,8 @@ package com.jinwoo.festa.presentation.di.module.main
 import android.content.Context
 import com.jinwoo.festa.data.Api
 import com.jinwoo.festa.data.database.EventDatabase
-import com.jinwoo.festa.data.datasource.EventDataSource
-import com.jinwoo.festa.data.datasource.EventDataSourceImpl
 import com.jinwoo.festa.data.mapper.EventDataMapper
 import com.jinwoo.festa.data.repository.EventRepositoryImpl
-import com.jinwoo.festa.domain.entity.EventRepository
-import com.jinwoo.festa.domain.entity.EventService
-import com.jinwoo.festa.domain.entity.EventServiceImpl
 import com.jinwoo.festa.domain.usecase.GetEventListUseCase
 import com.jinwoo.festa.presentation.di.scope.ActivityScope
 import com.jinwoo.festa.presentation.main.MainContract
@@ -19,19 +14,58 @@ import dagger.Module
 import dagger.Provides
 import io.reactivex.disposables.CompositeDisposable
 import androidx.room.Room
+import com.jinwoo.festa.data.database.LikeDatabase
+import com.jinwoo.festa.data.database.TicketDatabase
 import com.jinwoo.festa.data.database.dao.EventDao
+import com.jinwoo.festa.data.database.dao.LikeDao
+import com.jinwoo.festa.data.database.dao.TicketDao
+import com.jinwoo.festa.data.datasource.*
+import com.jinwoo.festa.data.mapper.LikeDataMapper
+import com.jinwoo.festa.data.mapper.TicketDataMapper
+import com.jinwoo.festa.data.repository.LikeRepositoryImpl
+import com.jinwoo.festa.data.repository.TicketRepositoryImpl
+import com.jinwoo.festa.domain.entity.*
 
 
 @Module
 class MainModule {
     @ActivityScope
     @Provides
+    fun provideTicketDatabase(context: Context): TicketDatabase
+            = Room.databaseBuilder(context, TicketDatabase::class.java, "ticket.db").build()
+
+    @ActivityScope
+    @Provides
     fun provideEventDatabase(context: Context): EventDatabase
             = Room.databaseBuilder(context, EventDatabase::class.java, "event.db").build()
+
+
+    @ActivityScope
+    @Provides
+    fun provideLikeDatabase(context: Context): LikeDatabase
+            = Room.databaseBuilder(context, LikeDatabase::class.java, "like.db").build()
 
     @ActivityScope
     @Provides
     fun provideEventDao(database: EventDatabase): EventDao = database.eventDao
+
+    @ActivityScope
+    @Provides
+    fun provideTicketDao(database: TicketDatabase): TicketDao = database.ticketDao
+
+    @ActivityScope
+    @Provides
+    fun provideLikeDao(database: LikeDatabase): LikeDao = database.likeDao
+
+    @ActivityScope
+    @Provides
+    fun provideLikeDataSource(likeDao: LikeDao): LikeDataSource
+            = LikeDataSourceImpl(likeDao)
+
+    @ActivityScope
+    @Provides
+    fun provideTicketDataSource(ticketDao: TicketDao): TicketDataSource
+            = TicketDataSourceImpl(ticketDao)
 
     @ActivityScope
     @Provides
@@ -40,13 +74,24 @@ class MainModule {
 
     @ActivityScope
     @Provides
+    fun provideLikeRepository(likeDataSource: LikeDataSource, likeDataMapper: LikeDataMapper): LikeRepository
+            = LikeRepositoryImpl(likeDataSource, likeDataMapper)
+
+    @ActivityScope
+    @Provides
     fun provideEventRepository(eventDataSource: EventDataSource, eventDataMapper: EventDataMapper): EventRepository
             = EventRepositoryImpl(eventDataSource, eventDataMapper)
 
     @ActivityScope
     @Provides
-    fun provideEventService(eventRepository: EventRepository): EventService
-            = EventServiceImpl(eventRepository)
+    fun provideTicketRepository(ticketDataSource: TicketDataSource, ticketDataMapper: TicketDataMapper): TicketRepository
+            = TicketRepositoryImpl(ticketDataSource, ticketDataMapper)
+
+    @ActivityScope
+    @Provides
+    fun provideEventService(eventRepository: EventRepository,
+                            likeRepository: LikeRepository, ticketRepository: TicketRepository): EventService
+            = EventServiceImpl(eventRepository, likeRepository, ticketRepository)
 
     @ActivityScope
     @Provides
@@ -59,7 +104,15 @@ class MainModule {
 
     @ActivityScope
     @Provides
+    fun provideLikeDataMapper(): LikeDataMapper = LikeDataMapper()
+
+    @ActivityScope
+    @Provides
     fun provideEventModelMapper(): EventModelMapper = EventModelMapper()
+
+    @ActivityScope
+    @Provides
+    fun provideTicketDataMapper() = TicketDataMapper()
 
     @ActivityScope
     @Provides
